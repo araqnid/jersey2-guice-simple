fd11fa339d3c03f730f9a0f0a3bba0b2c755a18e
Steve Haslam <araqnid@googlemail.com>
2015-11-24 02:34:37 +0000

    Bump Jersey to 2.22.1 - no longer any need to create JDK server using reflection

--- src/main/java/org/araqnid/jerseysimple/JdkHttpServerService.java
+++ src/main/java/org/araqnid/jerseysimple/JdkHttpServerService.java
@@ -1,18 +1,13 @@
 package org.araqnid.jerseysimple;
 
-import java.lang.reflect.Constructor;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
 import java.net.URI;
 import java.util.Set;
 
 import javax.inject.Inject;
-import javax.ws.rs.core.Application;
 import javax.ws.rs.core.UriBuilder;
 
 import org.glassfish.hk2.api.ServiceLocator;
 import org.glassfish.hk2.api.ServiceLocatorFactory;
-import org.glassfish.jersey.jdkhttp.JdkHttpHandlerContainer;
 import org.glassfish.jersey.jdkhttp.JdkHttpServerFactory;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.glassfish.jersey.server.monitoring.ApplicationEvent;
@@ -28,7 +23,6 @@ import com.google.common.collect.ImmutableSet;
 import com.google.common.util.concurrent.AbstractIdleService;
 import com.google.inject.Injector;
 import com.sun.net.httpserver.HttpServer;
-import com.sun.net.httpserver.HttpsConfigurator;
 
 public class JdkHttpServerService extends AbstractIdleService {
 	private static final Logger LOG = LoggerFactory.getLogger(JdkHttpServerService.class);
@@ -38,12 +32,13 @@ public class JdkHttpServerService extends AbstractIdleService {
 	@Inject
 	public JdkHttpServerService(Injector injector, @Port int port, @Resources Set<Class<?>> resourceClasses) {
 		uri = UriBuilder.fromUri("http://localhost/").port(port).build();
-		ResourceConfig resourceConfig = new ResourceConfig(ImmutableSet.<Class<?>> builder().addAll(resourceClasses)
-				.add(EventListener.class).build());
+		ResourceConfig resourceConfig = new ResourceConfig(
+				ImmutableSet.<Class<?>> builder().addAll(resourceClasses).add(EventListener.class).build());
 		ServiceLocator serviceLocator = ServiceLocatorFactory.getInstance().create("http-" + port);
 		GuiceBridge.getGuiceBridge().initializeGuiceBridge(serviceLocator);
 		serviceLocator.getService(GuiceIntoHK2Bridge.class).bridgeGuiceInjector(injector);
-		server = createHttpServer(uri, resourceConfig, serviceLocator, false);
+		server = JdkHttpServerFactory.createHttpServer(uri, resourceConfig, serviceLocator, /* sslContext */ null,
+				false);
 	}
 
 	@Override
@@ -76,27 +71,6 @@ public class JdkHttpServerService extends AbstractIdleService {
 			case ON_EXCEPTION:
 				LOG.error("exception handling request", event.getException());
 			}
-		}
-	}
-
-	// there is no method provided in JdkHttpServerFactory taking both parentLocator and start as parameters :(
-	private static HttpServer createHttpServer(URI uri,
-			ResourceConfig resourceConfig,
-			ServiceLocator parentLocator,
-			boolean start) {
-		try {
-			Constructor<JdkHttpHandlerContainer> constructor = JdkHttpHandlerContainer.class.getDeclaredConstructor(
-					Application.class, ServiceLocator.class);
-			constructor.setAccessible(true);
-			JdkHttpHandlerContainer handler = constructor.newInstance(resourceConfig, parentLocator);
-
-			Method factoryMethod = JdkHttpServerFactory.class.getDeclaredMethod("createHttpServer", URI.class,
-					JdkHttpHandlerContainer.class, HttpsConfigurator.class, Boolean.TYPE);
-			factoryMethod.setAccessible(true);
-			return (HttpServer) factoryMethod.invoke(null, uri, handler, null, start);
-		} catch (NoSuchMethodException | SecurityException | InstantiationException | IllegalAccessException
-				| IllegalArgumentException | InvocationTargetException e) {
-			throw new IllegalStateException("Unable to naughtily create server with reflection", e);
 		}
 	}
 }
